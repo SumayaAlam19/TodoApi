c675d403e803617d51bcad49ccb83613ca4aac2d
SumayaAlam19 <56950422+SumayaAlam19@users.noreply.github.com>
2020-08-26 10:26:16 +1000

    first commit

---
+++ TodoApi/Models/TodoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        //injecting the class// generic type/ of type TodoContext
+        public TodoContext(DbContextOptions<TodoContext> options) // 
+            : base(options)// passing it to base calling the const parent and passing the option
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
+
+//Today 
+
+//1
+//Install Angular
+//Done
+
+//2
+//Create web api 
+//Done
+
+//3
+//Install postman
+//TbD
+
+//4
+//Use postman to test the api
+//To be done
+
+//Tomorrow
+
+//5
+//Task 1
+//Done
+
+//6
+//Task2
+//Done
+
+//7
+//Task 3
+//In Progress
